f04f95422dfe4a0f62831b4eba5fdd97a4306e55
balaji24k <balaji325s@gmail.com>
2023-01-08 19:16:14 +0530

    asyncTask7Dynamic

--- DynamicRegPage/async.js
+++ DynamicRegPage/async.js
@@ -50,53 +50,53 @@
 // // console.log('person5: shows ticket'):
 
 function buycar(){
-  return new Promise((resolve,reject)=>{
-      setTimeout(()=>{
-          resolve("buy a car");
-      })
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("buy a car");
+        })
 
-  })
+    })
 }
 
 function plantoManali(){
-  return new Promise((resolve,reject)=>{
-      setTimeout(()=>{
-          resolve("lets plan to manali");
-      },1000);
-  })
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("lets plan to manali");
+        },1000);
+    })
 }
 
 function letscontibute(){
-  return new Promise((resolve,reject)=>{
-      setTimeout(()=>{
-          resolve("lets contribute");
-      },2000);
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("lets contribute");
+        },2000);
 
-  })
+    })
 }
 function GetfamousdestinationtoManal(){
-  return new Promise((resolve,reject)=>{
-      setTimeout(()=>{
-          reject("some accident happens")
-      },2000);
-  })
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            reject("some accident happens")
+        },2000);
+    })
 }
 async function fun1(){
-  try{
-      const msg1= await buycar();
-      console.log(msg1)
+    try{
+        const msg1= await buycar();
+        console.log(msg1)
 
-      const msg2 = await plantoManali();
-      console.log(msg2);
+        const msg2 = await plantoManali();
+        console.log(msg2);
 
-      const msg3 =await letscontibute();
-      console.log(msg3);
+        const msg3 =await letscontibute();
+        console.log(msg3);
 
-      const msg4 =await GetfamousdestinationtoManal();
-      console.log(msg4);
-  }catch(err){
-      console.log(err);
-  }
+        const msg4 =await GetfamousdestinationtoManal();
+        console.log(msg4);
+    }catch(err){
+        console.log(err);
+    }
 
 }
 fun1();
